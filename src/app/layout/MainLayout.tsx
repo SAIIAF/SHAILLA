bfde4b8786f6af0b75aa2f7e49332ebe4d0f45a7
Saif <khaledsaif194@gmail.com>
2026-04-13 05:21:08 +0200

    add scroll to top

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
+
 import './MainLayout.css';
 
 export const MainLayout: React.FC = () => {
   return (
     <div className="main-layout">
+      <ScrollToTop />
       <Navbar />
       <main className="main-content">
         <Outlet />
